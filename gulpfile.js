a0e849d3cac84df129911e1dc1dc485f8f046c7c
Paisit Jarunnamsiri <paisit.j@gmail.com>
2016-03-28 14:27:03 +0700

    Ch1

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
   connect = require('gulp-connect');
  
 gulp.task('webserver', function() {
-  connect.server();
+  connect.server({
+    livereload: true,
+    fallback: "d3ia.html"
+  });
 });
  
 gulp.task('default', ['webserver']);
